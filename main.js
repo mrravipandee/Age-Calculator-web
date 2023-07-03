15402f267d5516f98792ddb3a2b6b4fdce58b35c
Ravi Pandey <imravipanday@gmail.com>
2023-07-03 22:18:54 +0530

    JS code successfully i copy from chatGPT

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-//select output elements
+// Select output elements
 const output_year = document.querySelector(".output-year");
 const output_month = document.querySelector(".output-month");
 const output_day = document.querySelector(".output-day");
 
 const submit_btn = document.querySelector(".submit-button");
 
-//select input elements
-
+// Select input elements
 let isValid = false;
 
 const input_year = document.querySelector("#year");
@@ -18,43 +17,30 @@ const error_year = document.querySelector(".error-year");
 const error_month = document.querySelector(".error-month");
 const error_day = document.querySelector(".error-day");
 
-// write a program logic
-
+// Write program logic
 input_day.addEventListener("input", (e) => {
     const inputValue = +input_day.value; // Convert input value to a number
     
-    if (inputValue > 31) {
+    if (inputValue > 31 || inputValue < 1) {
         error_day.textContent = "Must be a valid date";
         isValid = false;
-        return;
-    } else if (inputValue === 0) {
-        error_day.textContent = "The field is required";
-        isValid = false;
-        return;
+    } else {
+        isValid = true;
+        error_day.textContent = "";
     }
-    
-    // If the input value is valid
-    isValid = true;
-    error_day.textContent = "";
 });
 
 
 input_month.addEventListener("input", (e) => {
     const inputValue = +input_month.value; // Convert input value to a number
     
-    if (inputValue > 12) {
+    if (inputValue > 12 || inputValue < 1) {
         error_month.textContent = "Must be a valid month";
         isValid = false;
-        return;
-    } else if (inputValue === 0) {
-        error_month.textContent = "The field is required";
-        isValid = false;
-        return;
+    } else {
+        isValid = true;
+        error_month.textContent = "";
     }
-    
-    // If the input value is valid
-    isValid = true;
-    error_month.textContent = "";
 });
 
 
@@ -63,25 +49,31 @@ input_year.addEventListener("input", (e) => {
 
     const currentYear = new Date().getFullYear();
 
-    if (inputValue > currentYear) {
+    if (inputValue > currentYear || inputValue < 1) {
         error_year.textContent = "Must be a valid year";
         isValid = false;
-        return;
-    } else if (inputValue === 0) {
-        error_year.textContent = "The field is required";
-        isValid = false;
-        return;
+    } else {
+        isValid = true;
+        error_year.textContent = "";
     }
-    
-    // If the input value is valid
-    isValid = true;
-    error_year.textContent = "";
 });
 
 submit_btn.addEventListener("click", function() {
     if (isValid) {
-        let birthday = '${input_month.value}/${input_day.value}/${input_year.value}';
+        const birthday = `${input_month.value}/${input_day.value}/${input_year.value}`;
+        const birthdayOb = new Date(birthday);
+        const ageDiffMill = Date.now() - birthdayOb.getTime();
+        const ageDate = new Date(ageDiffMill);
+        const ageYear = ageDate.getUTCFullYear() - 1970;
+        const ageMonth = ageDate.getUTCMonth();
+        const ageDay = ageDate.getUTCDate() - 1;
+
+        // Displaying
+        output_day.textContent = ageDay;
+        output_month.textContent = ageMonth;
+        output_year.textContent = ageYear;
+
     } else {
-        alert("error");
+        alert("Error: Please enter valid date values.");
     }
 });
